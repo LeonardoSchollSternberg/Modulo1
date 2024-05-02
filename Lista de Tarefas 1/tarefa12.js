bf8b4dd26ca2547ac50c5b955b78856927ef7674
LeonardoSchollSternberg <leossberg@gmail.com>
2024-05-02 09:03:10 -0300

    Revert "Este é um commit de teste"
    This reverts commit 5421601a95db8e32a2a007bd97c1b84c4b042e7d.

--- Lista de Tarefas 1/tarefa12.js
+++
@@ -1,9 +0,0 @@
-// 12. Escreva um algoritmo que gere os números de 1000 a 1999 e escreva aqueles que, divididos por 11, dão resto igual a 5.
-
-console.log("A seguir os números entre 1000 e 1999 que, divididos por 11, dão resto igual a 5");
-
-for (contador = 1000; contador < 2000; contador++) {
-    if(contador%11==5){
-        console.log(contador);
-    }
-}
